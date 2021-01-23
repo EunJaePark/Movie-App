4a17611815f8469cdebe1551547324194d3b713c
EunJaePark <design0728@naver.com>
2021-01-24 00:53:32 +0900

    update App, AppHeader
    - like button

--- src/App.vue
+++ src/App.vue
@@ -8,13 +8,8 @@
 <!--<script src="https://kit.fontawesome.com/aa7a88800a.js" crossorigin="anonymous"></script>-->
 
 <script>
-<<<<<<< HEAD
 import AppHeader from './components/common/AppHeader'; // 원래의 app header.
 // import AppHeader from './views/AppHeader'; // 좋아요 버튼 만들기 위해 생성한 views폴더 속의 app header.
-=======
-// import AppHeader from './components/common/AppHeader'; // 원래의 app header.
-import AppHeader from './views/AppHeader'; // 좋아요 버튼 만들기 위해 생성한 views폴더 속의 app header.
->>>>>>> d2b8d71f843016244b06d4f7811fa33c5c94fa9d
 // import MainPage from './views/MainPage';
 // import MoviePage from './views/MoviePage';
 
--- src/views/AppHeader.vue
+++ src/views/AppHeader.vue
@@ -1,27 +1,18 @@
 <template>
   <div class="appHeader">
       <Logo></Logo>
-<<<<<<< HEAD
-=======
       <LikeConts></LikeConts>
->>>>>>> d2b8d71f843016244b06d4f7811fa33c5c94fa9d
   </div>
 </template>
 
 <script>
 import Logo from '../components/common/Logo';
-<<<<<<< HEAD
-export default {
-    components: {
-        Logo,
-=======
 import LikeConts from '../components/common/LikeConts';
 
 export default {
     components: {
         Logo,
         LikeConts
->>>>>>> d2b8d71f843016244b06d4f7811fa33c5c94fa9d
     }
 }
 </script>
